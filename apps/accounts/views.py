59f898c764a3f7d82172d65756fd8eb2763e9dcf
Shathabdhi <shathabdhi03@gmail.com>
2026-04-09 16:53:36 +0530

    Added viewsets in accounts app

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+from .models import Parent, Student, Teacher, User
+from .serializers import (
+    ParentCreateSerializer,
+    StudentCreateSerializer,
+    TeacherCreateSerializer,
+    UserSerializer,
+)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherCreateSerializer
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentCreateSerializer
+
+
+class ParentViewSet(viewsets.ModelViewSet):
+    queryset = Parent.objects.all()
+    serializer_class = ParentCreateSerializer
